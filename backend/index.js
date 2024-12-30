d165a0cc8be236ba42c2222f1e096d99a5334367
jaypawar2004 <pawarjay7000@gmail.com>
2024-12-30 23:36:51 +0530

    PORTCHANGE

--- backend/index.js
+++ backend/index.js
@@ -606,7 +606,7 @@ app.get("/blog", isLoggedIn,async(req, res) => {
  }
  
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
