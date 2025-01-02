bf071f3139146365fe6714c2c701f9aa92547311
Jay Pawar <140436138+jaypawar2004@users.noreply.github.com>
2025-01-02 21:12:54 +0530

    Update StartHome.jsx

--- frontend/src/components/StartHome.jsx
+++ frontend/src/components/StartHome.jsx
@@ -20,7 +20,7 @@ const StartHome = ({ id, title, description, image }) => {
 
   useEffect(() => {
     // Fetch data from the backend
-    fetch("http://159.65.159.15/api/showproperties",{
+    fetch("/api/showproperties",{
        method: 'GET',
   headers: {
     'Cache-Control': 'no-cache'
