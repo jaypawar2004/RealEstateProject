c49fb30a2c08b2c4a77a3732a53d85908fade858
Jay Pawar <140436138+jaypawar2004@users.noreply.github.com>
2025-01-02 13:04:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,5 +7,14 @@ export default defineConfig({
   build: {
     outDir: 'dist', // Default 'dist', isko change karke 'build' kar diya
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://159.65.159.15:5000", // Backend URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 
 });
