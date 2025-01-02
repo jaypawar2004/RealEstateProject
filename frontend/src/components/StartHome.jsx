5a5755e0842fb10a07eacabb9577fe234ca9334f
Jay Pawar <140436138+jaypawar2004@users.noreply.github.com>
2025-01-02 12:21:44 +0530

    Update StartHome.jsx

--- frontend/src/components/StartHome.jsx
+++ frontend/src/components/StartHome.jsx
@@ -20,7 +20,7 @@ const StartHome = ({ id, title, description, image }) => {
 
   useEffect(() => {
     // Fetch data from the backend
-    fetch("/api/properties")
+    fetch("159.65.159.15:5000/api/properties")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Failed to fetch properties");
